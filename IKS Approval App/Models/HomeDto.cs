4ce7887d01abb70c616157c0d72cbd1cce7d2d27
IKSAD\Ronak.Suchak <ronak.suchak@ikshealth.com>
2021-04-08 15:29:50 +0530

    getByEmail DONE

---
+++ IKS Approval App/Models/HomeDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IKS_Approval_App.Models
+{
+    public class HomeDto
+    {
+        public int ApprovalId { get; set; }
+        public string Title { get; set; }
+    }
+}
