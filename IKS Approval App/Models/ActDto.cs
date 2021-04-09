938739a6e42a854fb1160a586d4e2e8467ba986e
IKSAD\Ronak.Suchak <ronak.suchak@ikshealth.com>
2021-04-09 16:11:49 +0530

    home count , act DONE

---
+++ IKS Approval App/Models/ActDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace IKS_Approval_App.Models
+{
+    public class ActDto
+    {
+        public int Approval_id { get; set; }
+        public string RecipientEmail { get; set; }
+        public string RecipientComment { get; set; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public Status Status { get; set; }
+        //public DateTime ApprovedDateTime { get; set; }
+    }
+}
---
+++ IKS Approval App/Models/CountDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IKS_Approval_App.Models
+{
+    public class CountDto
+    {
+        public int RecivedCount { get; set; }
+        public int SentCount { get; set; }
+
+    }
+}
