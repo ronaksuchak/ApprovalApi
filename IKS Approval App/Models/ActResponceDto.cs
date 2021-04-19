8ab7be7b5342384b58a5a1a025118fe4080dd24d
IKSAD\Ronak.Suchak <ronak.suchak@ikshealth.com>
2021-04-19 12:10:28 +0530

    act changed

---
+++ IKS Approval App/Models/ActResponceDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace IKS_Approval_App.Models
+{
+    public class ActResponceDto
+    {
+        public int ApprovalId { get; set; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public Status FinalStatus { get; set; }
+        public List<Recipient> Recipient { get; set; }
+        public String ApprovedTime { get; set; }
+
+    }
+}
