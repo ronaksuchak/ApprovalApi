1335c4857c05d8c29a7be37f36117940dae5d386
IKSAD\Ronak.Suchak <ronak.suchak@ikshealth.com>
2021-04-20 16:18:53 +0530

    Final Commit

---
+++ IKS Approval App/Models/ReminderDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IKS_Approval_App.Models
+{
+    public class ReminderDto
+    {
+        public int ApprovalId { get; set; }
+        public string Title { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
